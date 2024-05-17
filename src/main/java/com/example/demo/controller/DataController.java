1b74754bf091b213137af5404276710153a1ca26
JerryWang0122 <school832@gmail.com>
2024-05-17 15:29:45 +0800

    feat: get 個別輪船

--- src/main/java/com/example/demo/controller/DataController.java
+++ src/main/java/com/example/demo/controller/DataController.java
@@ -4,6 +4,7 @@ import com.example.demo.model.po.Ship;
 import com.example.demo.model.response.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,6 +41,23 @@ public class DataController {
     public ResponseEntity<ApiResponse<Ship>> getShip() {
         // 模擬船隻資料
         Ship ship = new Ship("Titanic", "郵輪", 280, 30);
+        ApiResponse<Ship> apiResponse = new ApiResponse<>(true, "success", ship);
+        return ResponseEntity.ok(apiResponse);
+    }
+
+    @GetMapping("/ship/{id}")
+    public ResponseEntity<ApiResponse<Ship>> getShipById(@PathVariable("id") Integer id) {
+        // 模擬多艘船隻資料
+        List<Ship> ships = List.of(
+                new Ship("Titanic", "郵輪", 280, 30),
+                new Ship("Evergreen", "貨輪", 200, 40),
+                new Ship("順風一號", "客船", 30, 10)
+        );
+        if (id >= ships.size() || id < 0) {
+            ApiResponse<Ship> apiResponse = new ApiResponse<>(false, "查無資料", null);
+            return ResponseEntity.ok(apiResponse);
+        }
+        Ship ship = ships.get(id);
         ApiResponse<Ship> apiResponse = new ApiResponse<>(true, "success", ship);
         return ResponseEntity.ok(apiResponse);
     }
