a0034c4d34b7d668200e17753e1256ecaf29da4f
JerryWang0122 <school832@gmail.com>
2024-05-17 13:41:43 +0800

    feat: ship Model

--- src/main/java/com/example/demo/controller/DataController.java
+++ src/main/java/com/example/demo/controller/DataController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
 
@@ -15,9 +16,12 @@ public class DataController {
 
     // 取得今日時間
     @GetMapping("/today")
-    public ResponseEntity<ApiResponse<Date>> today() {
+    public ResponseEntity<ApiResponse<String>> today() {
         Date today = new Date();
-        ApiResponse<Date> apiResponse = new ApiResponse<>(true, "success", today);
+        // "yyyy-MM-dd HH:mm:ss E" ,"yyyy-MM-dd a hh:mm:ss E"
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd a hh:mm:ss E");
+        String todayString = sdf.format(today);
+        ApiResponse<String> apiResponse = new ApiResponse<>(true, "success", todayString);
         return ResponseEntity.ok(apiResponse);
     }
 
