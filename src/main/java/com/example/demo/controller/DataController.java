40652bd941541c0e83d4461031aa3f9e79fb3fde
JerryWang0122 <school832@gmail.com>
2024-05-17 13:58:08 +0800

    feat: getShips

--- src/main/java/com/example/demo/controller/DataController.java
+++ src/main/java/com/example/demo/controller/DataController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.po.Ship;
 import com.example.demo.model.response.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Random;
 
 @RestController     // CSR
@@ -30,6 +32,28 @@ public class DataController {
     public ResponseEntity<ApiResponse<Integer>> lotto() {
         Integer data = new Random().nextInt(100);
         ApiResponse<Integer> apiResponse = new ApiResponse<>(true, "success", data);
+        return ResponseEntity.ok(apiResponse);
+    }
+
+    // 取得船隻資料
+    @GetMapping("/ship")
+    public ResponseEntity<ApiResponse<Ship>> getShip() {
+        // 模擬船隻資料
+        Ship ship = new Ship("Titanic", "郵輪", 280, 30);
+        ApiResponse<Ship> apiResponse = new ApiResponse<>(true, "success", ship);
+        return ResponseEntity.ok(apiResponse);
+    }
+
+    // 取得多筆船隻資料
+    @GetMapping("/ships")
+    public ResponseEntity<ApiResponse<List<Ship>>> getShips() {
+        // 模擬多艘船隻資料
+        List<Ship> ships = List.of(
+                new Ship("Titanic", "郵輪", 280, 30),
+                new Ship("Evergreen", "貨輪", 200, 40),
+                new Ship("順風一號", "客船", 30, 10)
+        );
+        ApiResponse<List<Ship>> apiResponse = new ApiResponse<>(true, "success", ships);
         return ResponseEntity.ok(apiResponse);
     }
 }
