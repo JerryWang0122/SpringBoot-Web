62272cfa41a4597940a6a57aa1e316e4931ce30b
JerryWang0122 <school832@gmail.com>
2024-05-20 11:11:44 +0800

    feat: lotto 查詢 (async)

--- src/main/resources/static/js/promise3.js
+++ src/main/resources/static/js/promise3.js
@@ -13,4 +13,20 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log('do something~~')
         console.log('end');
     })
+
+    // 非同步
+    $('queryButton2').addEventListener('click', (event) => {
+        console.log('begin');
+
+        fetch('http://localhost:8081/data/lotto')
+            .then(response => response.json())
+            .then(({ state, message, data }) => {
+                console.log(state, message, data);
+                console.log('end');
+            });
+
+        // 做其他事情(非同步)
+        console.log('do something~~');
+    })
+
 })
